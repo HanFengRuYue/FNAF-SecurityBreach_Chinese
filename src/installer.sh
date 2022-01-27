f0674c4d27ccc33001d183881c45c2eeaddb1845
ShulkerSakura <50770360+ShulkerSakura@users.noreply.github.com>
2022-01-28 01:28:00 +0800

    Update installer.sh

--- src/installer.sh
+++ src/installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'This script must running under the directory "~/Quarters/"' 
-wget https://github.com/HanFengRuYue/FNAF-SecurityBreach_Chinese/releases/download/V2.3/FNAF.SB_2.3.pak
-mv FNAF.SB_2.3.pak ./fnaf9/Content/Paks/fnaf9-WindowsNoEditor_p.pak
+wget https://github.com/HanFengRuYue/FNAF-SecurityBreach_Chinese/releases/download/V2.7/FNAF.SB_2.7.pak
+mv FNAF.SB_2.7.pak ./fnaf9/Content/Paks/fnaf9-WindowsNoEditor_p.pak
 echo "Done"
