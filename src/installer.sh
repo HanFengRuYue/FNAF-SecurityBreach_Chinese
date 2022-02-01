029f253e4bebc4b0249295b42d8270ea02f8d6f1
ShulkerSakura <2531493755@qq.com>
2022-02-01 19:21:10 +0800

    v3.0 linux script

---
+++ src/installer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'This script must running under the directory "~/Quarters/"' 
+wget https://github.com/HanFengRuYue/FNAF-SecurityBreach_Chinese/releases/download/Install_v3.0/FNAF.SB_3.0.pak
+mv FNAF.SB_3.0.pak ./fnaf9/Content/Paks/fnaf9-WindowsNoEditor_p.pak
+echo "Done"
--- 源代码/installer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 'This script must running under the directory "~/Quarters/"' 
-wget https://github.com/HanFengRuYue/FNAF-SecurityBreach_Chinese/releases/download/Install_v2.9.1/FNAF.SB_2.9.1.pak
-mv FNAF.SB_2.9.1.pak ./fnaf9/Content/Paks/fnaf9-WindowsNoEditor_p.pak
-echo "Done"
